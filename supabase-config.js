a7f30655395cb00297b9722deaa21d49effea2e1
adzon812-ops <adzon812@gmail.com>
2026-02-22 14:05:15 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Настройки Supabase (потом заменим на свои)
-const SUPABASE_URL = 'https://your-project.supabase.co';
-const SUPABASE_KEY = 'your-anon-key';
+const SUPABASE_URL = 'https://qagcjbfqjheflilennpd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZ2NqYmZxamhlZmxpbGVubnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2ODI5MzksImV4cCI6MjA4NzI1ODkzOX0.jzLXUE6Zi0EbcmorXxSqlLBVIRylJyYIeTQd5Kan53I';
 
 // Функция для подключения
 function initSupabase() {
